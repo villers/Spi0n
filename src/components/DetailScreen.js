7cf22c49ad36ace0b2fecc392b488460c528b480
VILLERS Mickaël <mickael.villers@epitech.eu>
2017-03-16 01:46:49 +0100

    Fix loading spin when DetailScreen did mount

--- src/components/DetailScreen.js
+++ src/components/DetailScreen.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { View, Text, StyleSheet, WebView, Platform } from 'react-native';
+import { View, Text, StyleSheet, WebView, Platform, InteractionManager } from 'react-native';
 import { Spinner } from 'native-base';
 
 const styles = StyleSheet.create({
@@ -20,40 +20,44 @@ const styles = StyleSheet.create({
 class DetailScreen extends React.PureComponent {
 
   componentDidMount() {
-    this.loadContent();
-  }
-
-  loadContent() {
     const { id } = this.props;
 
-    this.props.fetchData(`https://www.spi0n.com/wp-json/nq/v1/post/${id}`);
+    this.loaded = false;
+
+    InteractionManager.runAfterInteractions(() => {
+      this.props.fetchData(`https://www.spi0n.com/wp-json/nq/v1/post/${id}`);
+      this.loaded = true;
+    });
+  }
+
+  isLoading() {
+    const { isLoading } = this.props;
+
+    return !this.loaded || isLoading;
   }
 
   render() {
-    const { item, isLoading } = this.props;
+    const { item } = this.props;
     const startInLoadingState = Platform.OS !== 'ios';
 
     const videoUrl = `https://www.dailymotion.com/embed/video/${item.url_dailymotion}?wmode=transparent&autoplay=1`;
 
+    if (this.isLoading()) {
+      return <Spinner />;
+    }
 
     return (
       <View style={styles.container}>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <View style={styles.container}>
-            <WebView
-              source={{ uri: videoUrl }}
-              allowsInlineMediaPlayback
-              startInLoadingState={startInLoadingState}
-            />
-            <View style={styles.padding}>
-              <View style={styles.background}>
-                <Text style={styles.text}>{item.post_title}</Text>
-              </View>
-            </View>
+        <WebView
+          source={{ uri: videoUrl }}
+          allowsInlineMediaPlayback
+          startInLoadingState={startInLoadingState}
+        />
+        <View style={styles.padding}>
+          <View style={styles.background}>
+            <Text style={styles.text}>{item.post_title}</Text>
           </View>
-        )}
+        </View>
       </View>
     );
   }
